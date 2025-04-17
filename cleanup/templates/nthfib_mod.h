d5a161898dfc95c2c752d61e068fb447427f7984
mhnd <mhnd@m.com>
2025-04-17 20:34:20 +0200

    linux

--- cleanup/templates/nthfib_mod.h
+++ cleanup/templates/nthfib_mod.h
@@ -9,3 +9,15 @@ pair<Mint, Mint> fib (ll n) { // return f_n, f_n+1
     else
         return {c, d};
 }
+
+pair<ll, ll> fib(ll n, ll MOD) { // return f_n, f_n+1
+    if (n == 0)
+        return {0, 1};
+    auto p = fib(n >> 1, MOD);
+    ll c = (p.first * (2 * p.second % MOD - p.first + MOD)) % MOD;
+    ll d = (p.first * p.first % MOD + p.second * p.second % MOD) % MOD;
+    if (n & 1)
+        return {d, (c + d) % MOD};
+    else
+        return {c, d};
+}
